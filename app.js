b53159fbe176abbfcd71a47df4b98618f6050f3a
MaricarmenCatalinaRaymundoRomero <129924045+MaricarmenCatalinaRaymundoRomero@users.noreply.github.com>
2023-06-11 14:48:53 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+const paypalBtn = document.getElementById('paypal-btn');
+
+paypalBtn.addEventListener('click', function(event) {
+  event.preventDefault();
+  const nombre = document.getElementById('nombre').value;
+  const apellido = document.getElementById('apellido').value;
+  const correo = document.getElementById('correo').value;
+  const monto = document.getElementById('monto').value;
+  const url = `https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&business=${correo}&amount=${monto}&item_name=${nombre} ${apellido}&currency_code=USD`;
+  window.open(url);
+});
+
+
+const btnSwitch = document.querySelector('#switch');
+
+btnSwitch.addEventListener('click', ()=> {
+    document.body.classList.toggle('dark');
+    btnSwitch.classList.toggle('active');
+});
